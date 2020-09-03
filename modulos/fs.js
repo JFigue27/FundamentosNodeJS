18bd94f075b4bf4da1f53c5b989e47732783a8fb
jjfigueroa <josejaime.figueroa@molex.com>
2020-09-03 11:12:31 -0600

    files system functions...

---
+++ modulos/fs.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+
+// console.log(__dirname + '\\archivo.txt');
+
+const read = (path, cb) => {
+  fs.readFile(path, (err, data) => {
+      console.log(path);
+    //..
+    cb(data.toString());
+  });
+}
+
+
+const write = (path, data, cb) => {
+    fs.writeFile(path, data, (err) =>{
+        //..something
+        if (err) {
+            console.log('Somethig went wrong...');
+        } else {
+            console.log('The file was written successfully');
+        }
+    })
+}
+
+const deleteFile = (path, cb) => {
+    fs.unlink(path, cb)
+
+}
+
+// write(__dirname + '\\archivo-2.txt', 'I am a new file...', console.log);
+// read(__dirname + '\\archivo.txt', console.log);
+deleteFile(__dirname + '\\archivo-2.txt', console.log);
