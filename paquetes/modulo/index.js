38625ab4cde4019d5edb83848357a99842e18db6
jjfigueroa <josejaime.figueroa@molex.com>
2020-09-04 14:33:21 -0600

    require modules from nodeJS

---
+++ paquetes/modulo/index.js
@@ -0,0 +1,10 @@
+// Traer nuestro modulo
+const modulo = require('./modulo');
+
+// Ejecutar una funcion del modulo
+// console.log(modulo);
+
+// modulo();
+
+console.log(modulo.prop1);
+modulo.saludar();
